1ba346c8fb0dbfb114824c83491699479ed5d4a9
Andrew Doing <andrewsdoing@gmail.com>
2020-08-25 10:46:25 -0700

    Add doc comments to kustomize.go

--- cmd/kustomize.go
+++ cmd/kustomize.go
@@ -21,6 +21,8 @@ const kustomizationFileName = "kustomization.yaml"
 const defaultKind = "Kustomization"
 const defaultAPIVersion = "kustomize.config.k8s.io/v1beta1"
 
+// setDefaultEmptyKustomization sets k.Kind and k.APIVersion to the constant default values, and
+// k.Resources to an empty string slice
 func (k *kustomization) setDefaultEmptyKustomization() {
 	if k.Kind == "" {
 		k.Kind = defaultKind
@@ -33,6 +35,7 @@ func (k *kustomization) setDefaultEmptyKustomization() {
 	}
 }
 
+// addKustomizationResource appends one LogicalPath/component.yaml to k.Resources
 func (k *kustomization) addKustomizationResource(component core.Component) {
 	componentYAMLFilename := fmt.Sprintf("%s.yaml", component.Name)
 	componentYAMLFilepath := path.Join(component.LogicalPath, componentYAMLFilename)
@@ -40,12 +43,15 @@ func (k *kustomization) addKustomizationResource(component core.Component) {
 	k.Resources = append(k.Resources, componentYAMLFilepath)
 }
 
+// writeKustomizationFile writes a yaml byte slice to the kustomization file in the generationPath
 func writeKustomizationFile(generationPath string, kustomizationBytes []byte) (err error) {
 	kustomizationFile := path.Join(generationPath, kustomizationFileName)
 	logger.Info(emoji.Sprintf(":floppy_disk: Writing %s", kustomizationFile))
 	return ioutil.WriteFile(kustomizationFile, kustomizationBytes, 0644)
 }
 
+// createKustomizationFile is composed of all the other kustomize functions, fulfilling its namesake by
+// creating the object, adding resources, marshalling the data to yaml, and writing the file.
 func createKustomizationFile(generationPath string, components []core.Component) (err error) {
 	kustomization := kustomization{}
 	kustomization.setDefaultEmptyKustomization()
